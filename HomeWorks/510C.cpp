c4393e745340208298482bc83fe2d1389bb7c682
azecoder <tabriz.haji@gmail.com>
2019-01-09 23:07:28 +0100

    graph alg, bfs, dfs - hw 05/11/2018

--- HomeWorks/510C.cpp
+++ HomeWorks/510C.cpp
@@ -8,12 +8,8 @@
 #define rrange(i,a,b)   for(int i=a;i>=b;i--)
 #define rangev(i,a)     for(int i=0;i<a.size();i++)
 #define rrangev(i,a)    for(int i=a.size()-1;i>=0;i--)
-#define clr(a)          memset(a, 0, sizeof(a))
-#define all(a)          a.begin(), a.end()
 #define mp              make_pair
 #define pb              push_back
-#define st              first
-#define nd              second
 #define MX              200005
 
 
@@ -21,13 +17,6 @@ using namespace std;
 
 typedef long long ll;
 typedef vector <int> vi;
-typedef pair <int, int> pii;
-typedef vector <pii> vii;
-typedef pair <int, pii> pip;
-
-const ll INF = 1e9 + 100;
-const ll MOD = 1e9 + 7;
-
 
 int toAlphIndex(char ch) {
     return int(ch - 'a' + 1);
