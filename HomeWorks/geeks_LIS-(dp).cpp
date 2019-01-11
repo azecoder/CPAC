40c12c337937a484b3133eea719ddbf251a8e0e5
azecoder <tabriz.haji@gmail.com>
2019-01-11 09:52:39 +0100

    longest increasing subsequence - hw 27/11/2018

--- HomeWorks/geeks_LIS-(dp).cpp
+++ HomeWorks/geeks_LIS-(dp).cpp
@@ -2,18 +2,18 @@
 
 // defines
 #define faster_io       ios_base::sync_with_stdio(false);cin.tie()
-#define rep(i,a)        for(int i=0;i<a;i++)
-#define rrep(i,a)       for(int i=a-1,i>=0;i--)
 #define range(i,a,b)    for(int i=a;i<=b;i++)
 #define rrange(i,a,b)   for(int i=a;i>=b;i--)
 #define rangev(i,a)     for(int i=0;i<a.size();i++)
 #define rrangev(i,a)    for(int i=a.size()-1;i>=0;i--)
+#define rangem(x,v)     for(typeof (v).begin() x=(v).begin();x!=(v).end();++x)
 #define clr(a)          memset(a, 0, sizeof(a))
 #define all(a)          a.begin(), a.end()
 #define mp              make_pair
 #define pb              push_back
 #define st              first
 #define nd              second
+#define MX              300005
 
 
 using namespace std;
@@ -22,48 +22,43 @@ typedef long long ll;
 typedef vector <int> vi;
 typedef pair <int, int> pii;
 typedef vector <pii> vii;
-typedef pair <int, pii> pip;
+typedef pair<double, int> pdi;
+typedef vector<pdi> vdi;
 
 const ll INF = 1e9 + 100;
 const ll MOD = 1e9 + 7;
 
 
-string str1, str2;
+int LIC(int N, int *arr) {
 
-int editDistance(int N, int M) {
+    int *lic = new int [N + 1];
 
-    if(N == 0) {
-        return M;
-    }
-    if(M == 0) {
-        return N;
-    }
-
-    if(str1[N] == str2[M]) {
-        return editDistance(N-1, M-1);
+    lic[1] = 1;
+    range(i, 2, N) {
+        range(j, 1, i - 1) {
+            if(arr[i] > arr[j]) {
+                lic[i] = max(lic[i], lic[j] + 1);
+            }
+        }
     }
 
-    return 1 + min(
-               editDistance(N, M - 1),
-               min(
-                   editDistance(N - 1, M),
-                   editDistance(N - 1, M - 1)
-               )
-           );
+    return lic[N];
 
 }
 
-int T, N, M;
+
+int N;
 
 int main() {
 
     faster_io;
 
-    cin >> T;
-    range(i, 1, T) {
-        cin >> N >> M >> str1 >> str2;
-        cout << editDistance(N - 1, M - 1) << "\n";
-    }
+    cin >> N;
+
+    int *arr = new int[N+1];
+    range(i, 1, N) cin >> arr[i];
+
+    cout << LIC(N, arr);
 
     return 0;
 }
--- HomeWorks/geeks_Minimum-number-of-jumps.cpp
+++ HomeWorks/geeks_Minimum-number-of-jumps.cpp
@@ -2,18 +2,18 @@
 
 // defines
 #define faster_io       ios_base::sync_with_stdio(false);cin.tie()
-#define rep(i,a)        for(int i=0;i<a;i++)
-#define rrep(i,a)       for(int i=a-1,i>=0;i--)
 #define range(i,a,b)    for(int i=a;i<=b;i++)
 #define rrange(i,a,b)   for(int i=a;i>=b;i--)
 #define rangev(i,a)     for(int i=0;i<a.size();i++)
 #define rrangev(i,a)    for(int i=a.size()-1;i>=0;i--)
+#define rangem(x,v)     for(typeof (v).begin() x=(v).begin();x!=(v).end();++x)
 #define clr(a)          memset(a, 0, sizeof(a))
 #define all(a)          a.begin(), a.end()
 #define mp              make_pair
 #define pb              push_back
 #define st              first
 #define nd              second
+#define MX              300005
 
 
 using namespace std;
@@ -22,48 +22,30 @@ typedef long long ll;
 typedef vector <int> vi;
 typedef pair <int, int> pii;
 typedef vector <pii> vii;
-typedef pair <int, pii> pip;
+typedef pair<double, int> pdi;
+typedef vector<pdi> vdi;
 
 const ll INF = 1e9 + 100;
 const ll MOD = 1e9 + 7;
 
 
-string str1, str2;
-
-int editDistance(int N, int M) {
-
-    if(N == 0) {
-        return M;
-    }
-    if(M == 0) {
-        return N;
-    }
-
-    if(str1[N] == str2[M]) {
-        return editDistance(N-1, M-1);
-    }
-
-    return 1 + min(
-               editDistance(N, M - 1),
-               min(
-                   editDistance(N - 1, M),
-                   editDistance(N - 1, M - 1)
-               )
-           );
-
-}
-
-int T, N, M;
+int N;
 
 int main() {
 
     faster_io;
 
-    cin >> T;
-    range(i, 1, T) {
-        cin >> N >> M >> str1 >> str2;
-        cout << editDistance(N - 1, M - 1) << "\n";
+    cin >> N;
+
+    int *arr = new int[N+1];
+    range(i, 1, N) cin >> arr[i];
+
+    int idx = 1, result = 0;
+    while(idx <= N) {
+        result++;
+        idx += arr[idx];
     }
+    cout << result;
 
     return 0;
 }
